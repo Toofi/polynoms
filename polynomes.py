1a51233a50849d204e71e1a57e70837a0ec21d7c
Toofi <herpoel.quentin@gmail.com>
2020-01-21 13:15:50 +0100

    addit and soustract ok

--- polynomes.py
+++ polynomes.py
@@ -1,36 +1,52 @@
 def createPolynom(degree = int):
-    polynom1 = []
-    for i in range(0,(int(degree) - 1)):
-        print("Votre indice pour le degré",i," :")
-        polynom1[i] = input()
-    print(polynom1)
+    polynom = [0]*(int(degree) + 1)
+    for i in range(0,(int(degree) + 1)):
+        print("Votre indice pour X ^",i," :")
+        polynom[i] = input()
+    return polynom
 
 def operateOnPolynoms(polynomA = list, polynomB = list, operation = str):
     polynomC = []
-    if len(polynomA)>len(polynomB): polynomC = [0]*len(polynomA)
-    else: polynomC = [0]*len(polynomB)
+    if len(polynomA)>len(polynomB): polynomC = [float(0)]*len(polynomA)
+    else: polynomC = [float(0)]*len(polynomB)
     if operation == "+":
         for i in range(len(polynomA)): polynomC[i] += polynomA[i]
         for i in range(len(polynomB)): polynomC[i] += polynomB[i]
-    if operation == "-":
+    elif operation == "-":
         for i in range(len(polynomA)): polynomC[i] += polynomA[i]
         for i in range(len(polynomB)): polynomC[i] -= polynomB[i]
-    if operation == "*":
+    elif operation == "*":
         for i in range(len(polynomA)): polynomC[i] += polynomA[i]
         for i in range(len(polynomB)): polynomC[i] * polynomB[i]
-        #a vérifier !
     else: print("Vous n'avez pas sélectionné le bon caractère pour effectuer une opération!")
-    print(polynomC)
+    return polynomC
+
+def polynomView(polynom = list):
+    for i in range(len(polynom)): 
+        print("",modifySign(polynom[i]),polynom[i],"X ^",i, end="")
+
+def modifySign(number = float):
+    if (float(number) >= 0):
+        result = "+"
+    else:
+        result = ""
+    return result
+
 
 print("Vous pouvez effectuer des opérations sur les polynomes en choisissant les opérations suivantes : + pour addition, - pour soustraction, * pour multiplication")
 operation = input("Il est temps de choisir : ")
 
-degre = input("Sélectionner le degré de votre polynome : ")
+degre00 = input("Sélectionner le degré de votre premier polynome : ")
+polynom00 = createPolynom(degre00)
+polynom00Float = list(map(float, polynom00))
+print("Voici votre premier polynome :",polynom00Float)
 
-createPolynom(degre)
+degre01 = input("Sélectionner le degré de votre second polynome : ")
+polynom01 = createPolynom(degre01)
+polynom01Float = list(map(float, polynom01))
+print("Voici votre second polynome :", polynom01Float)
 
-polynom00 = [-2,5,0,-2,0,1]
-polynom01 = [-4,0,3,0,1]
+print("Voici le résultat de l'opération :",operateOnPolynoms(polynom00Float, polynom01Float, operation))
 
-
-operateOnPolynoms(polynom00, polynom01, operation)
+print("Et le voici sous forme d'équation :")
+polynomView(operateOnPolynoms(polynom00Float, polynom01Float, operation))
